af5411a903f1c4d6f1bb0f6b62facddb7ad9a953
Suhani Jain <suhani.j2002@gmail.com>
2024-04-24 14:13:38 +0530

    Add files via upload
    Completed Tutorial till 24 - I

---
+++ Tutorial_17.js
@@ -0,0 +1,11 @@
+current_number = 0
+
+function increaseCount() {
+  current_number = current_number + 1;
+  document.getElementById('number').innerHTML = current_number;
+}
+
+function decreaseCount() {
+  current_number = current_number - 1;
+  document.getElementById('number').innerHTML = current_number;
+}
---
+++ Tutorial_18.js
@@ -0,0 +1,14 @@
+function calculateBMI() {
+    var weightInKg = document.getElementById('weight').value;
+    var heightInCm = document.getElementById('height').value;
+  
+    weightInKg = parseFloat(weightInKg);
+    heightInCm = parseFloat(heightInCm);
+  
+    heightInMeters = heightInCm / 100;
+  
+    var bmi = (weightInKg) / (heightInMeters * heightInMeters);
+    var text = "";
+    document.getElementById("result").innerHTML = "Your BMI is " + bmi.toFixed(2) 
+   
+  }
